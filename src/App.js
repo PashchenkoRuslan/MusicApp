f1b87040c487345a128decc55f2e14b1abbd1aa1
Ruslan Pashchenko <57391313+PashchenkoRuslan@users.noreply.github.com>
2023-06-01 11:38:16 +0500

    first app

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+const RegistrationForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Отправка данных на бэкэнд
+    // Здесь вы можете использовать fetch или другую библиотеку для выполнения HTTP-запроса
+    fetch('/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Обработка ответа от бэкэнда
+        console.log(data);
+      })
+      .catch((error) => {
+        // Обработка ошибок
+        console.error('Error:', error);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default RegistrationForm;
